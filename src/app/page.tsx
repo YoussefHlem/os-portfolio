4d56c4a2f48124ee4670801267563d014cbabbe3
YoussefHlem <youssefhlemdev@gmail.com>
2026-04-11 05:13:07 +0200

    Add Dock component and integrate into main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Welcome from "@/components/Welcome";
+import Dock from "@/components/Dock";
 
 export default function Home() {
   return (
-      <main>
-          <Navbar />
-          <Welcome />
-      </main>
+    <main>
+      <Navbar />
+      <Welcome />
+      <Dock />
+    </main>
   );
 }
---
+++ src/components/Dock.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dock = () => {
+  return <div></div>;
+};
+
+export default Dock;
