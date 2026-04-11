ae883103b47ae07ff9a7bb6f5f9fb64224ab1597
YoussefHlem <youssefhlemdev@gmail.com>
2026-04-11 03:15:39 +0200

    Fix font variable names in fonts.ts and globals.css

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,11 +3,11 @@ import { Georama, Roboto_Mono } from "next/font/google";
 export const georama = Georama({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-georama-family",
+  variable: "--font-georama",
 });
 
 export const robotoMono = Roboto_Mono({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-roboto-family",
+  variable: "--font-roboto",
 });
