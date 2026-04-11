f5c2618be4187b6b9895f0cef08d60835f5ec793
YoussefHlem <youssefhlemdev@gmail.com>
2026-04-12 00:42:04 +0200

    Add Terminal window component with window controls and integrate into main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@ import Navbar from "@/components/Navbar";
 import Welcome from "@/components/Welcome";
 import Dock from "@/components/Dock";
 
+import { Draggable } from "gsap/Draggable";
+import gsap from "gsap";
+import TerminalWindow from "@/windows/Terminal";
+
+gsap.registerPlugin(Draggable);
+
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Welcome />
       <Dock />
+
+      {/*  Windows*/}
+      <TerminalWindow />
     </main>
   );
 }
